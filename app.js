9d11e4c4ace948e28c1ee9ce2178a5db436ebdd0
LuigiCff <luigi.cleffi@nbwdigital.com.br>
2022-12-23 10:19:05 -0300

    updated named expressions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { getDate } from './views/date.js';
 import {express} from 'express'
 import {bodyParser} from "body-parser"
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -9,43 +10,48 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
-
-const todos = [];
-const workItems = [];
-
-app.get('/', (req, res) => {
-    let day = getDate();
-
-    res.render('list', {
-        listTitle: day,
-        newListItems: todos
-    })
-
-    res.send();
-})
-
-app.post('/', (req, res) => {
-    let { newTodo } = req.body;
-
-    if(req.body.list === "Work"){
-        workItems.push(newTodo)
-        res.redirect("/work");
-    }else{
-        todos.push(newTodo);
-        res.redirect("/");
-    }
+try{
+    const todos = [];
+    const workItems = [];
     
-})
-
-app.get("/work", (req,res) => {
-    res.render("list", {
-        listTitle: "Work List", 
-        newListItems: workItems
+    app.get('/', (req, res) => {
+        let day = getDate();
+    
+        res.render('list', {
+            listTitle: day,
+            newListItems: todos
+        })
+    
+        res.send();
     })
-}) 
-app.post("/work", (req, res)=> {
-    let {newItem} = req.body
-    workItems.push(newItem);
-    res.redirect("/work");
-})
+    
+    app.post('/', (req, res) => {
+        let { newTodo } = req.body;
+    
+        if(req.body.list === "Work"){
+            workItems.push(newTodo)
+            res.redirect("/work");
+        }else{
+            todos.push(newTodo);
+            res.redirect("/");
+        }
+        
+    })
+    
+    app.get("/work", (req,res) => {
+        res.render("list", {
+            listTitle: "Work List", 
+            newListItems: workItems
+        })
+    }) 
+    app.post("/work", (req, res)=> {
+        let {newItem} = req.body
+        workItems.push(newItem);
+        res.redirect("/work");
+    })
 app.listen(port, () => console.log("Running on port " + port))
+
+}catch(err){
+    ErrorCaptureStackTrace(err);
+    console.log(err);
+}
